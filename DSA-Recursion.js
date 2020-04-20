4c3b3a1bbea47014b669752426440ab11a2380c1
Bowie <bowie@thinkful.com>
2020-04-19 17:07:32 -0700

    init commit

---
+++ DSA-Recursion.js
@@ -0,0 +1,102 @@
+// 1. Counting Sheep
+// What is the input to the program?
+// Input is 3
+// What is the output of the program?
+// Output is to log "Another sheep jumped over the fence."
+// What is the input to each recursive call?
+// Input is input number - 1
+// What is the output of each recursive call?
+// Output is to log "Another sheep jumped over the fence." 
+// until the input has reached 0, which then logs "All sheep jumped over the fence"
+
+const countingSheep = function(numOfSheep) {
+    if (numOfSheep === 0) {
+        console.log('All sheep jumped over the fence') 
+    } else {
+        console.log('Another sheep jumped over the fence.') // output
+        return countingSheep(numOfSheep - 1) // changes each recursive call by -1
+    }
+}
+
+let numOfSheep = 3 // input to the program
+countingSheep(numOfSheep)
+
+// ========================================================
+// 2. Power Calculator
+// What is the input to the program?
+// 10 is the base, 2 is the exponent
+// What is the output of the program?
+// 100
+// What is the input to each recursive call?
+// base, exponent - 1
+// What is the output of each recursive call?
+// base * base
+
+const powerCalculator = function(base, expo) {
+    if (expo < 0) {
+        return "exponent should be >= 0";
+    } else if (expo === 0) {
+        return 1;
+    }
+    else {
+        return base * powerCalculator(base, expo-1);
+    }
+}
+
+console.log(powerCalculator(10, 2));
+
+// ========================================================
+// 3. Reverse String
+// What is the input to the program?
+// 'hello world'
+// What is the output of the program?
+// 'dlrow olleh'
+// What is the input to each recursive call?
+// Each time the first letter of each input should be cut while 
+// simultaneously added to the end of the word
+// What is the output of each recursive call?
+// The first letter of the string is taken out, then added to the end
+
+const reverseString = function(string) {
+    if (string.length < 2) {
+        return string;
+    }
+    return reverseString(string.slice(1)) + string[0]
+}
+reverseString('hello world');
+
+// ========================================================
+// 4. nth Triangular Number
+// What is the input to the program?
+// 5
+// What is the output of the program?
+// 15
+// What is the input to each recursive call?
+// n * (n + 1) / 2
+// What is the output of each recursive call?
+// 1, 3, 6, 10, 15
+
+function triangleNum(n) {
+    if (n < 2) {
+        return n;
+    }
+    return n + triangleNum(n - 1)
+}
+
+triangleNum(5);
+
+// ========================================================
+// 5. String Splitter
+// Input: '04/19/2020'
+// Output: ["04", "19", "2020"]
+
+function split(str, sep) {
+    let idx = str.idxOf(sep)
+    if ( idx == -1){
+        return [str];
+    }
+    // return [str.slice(0, idx)].concat(split(str.slice(idx + sep.length), sep))
+    return str.slice(0,idx).concat(split(str.slice(idx + sep.length), sep))
+}
+
+split('04/19/2020')
